db97fadea08bbeaeec3cd6fe99a49799e411ca36
Chinnatip Taemkaeo <chinnatip@me.com>
2018-07-31 00:08:02 +0700

    fixed

--- src/assets/linkRouter.js
+++ src/assets/linkRouter.js
@@ -1,5 +1,4 @@
 export const linkRouter = [
   { id: '123445', url: 'https://www.pantip.com' },
-  { id: '503514', url: 'https://www.kohlife.com' },
-  { id: '864312', url: 'https://www.supergeniusschool.com' }
+  { id: '503514', url: 'https://www.kohlife.com' }
 ]
